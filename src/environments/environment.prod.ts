516a95308efc0404a5138d6b4a8081cc10dcb82e
Alline <alline.oliveira@gmail.com>
2019-11-16 17:47:01 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,13 +4,13 @@ export const environment = {
   apiUrl: 'https://appbasicao.herokuapp.com',
 
   firebase: {
-    apiKey: 'AIzaSyA88q8BPd-yDT09f20-rAb3HnjM1nasi6g',
+    apiKey: 'XXXXXXXXXXXXXXXX',
     authDomain: 'appbasicao-33a2c.firebaseapp.com',
     databaseURL: 'https://appbasicao-33a2c.firebaseio.com',
     projectId: 'appbasicao-33a2c',
     storageBucket: 'appbasicao-33a2c.appspot.com',
     messagingSenderId: 'XXXXXXXXXXXXX',
-    appId: '1:114476927212:web:41e777537792e8c0a5fdc9',
+    appId: 'XXXXXXXXXXXXXXXX',
     measurementId: 'XXXXXXXXXXXXXXXX'
   }
 };
